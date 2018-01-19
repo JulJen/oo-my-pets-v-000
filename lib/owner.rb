09d2dd6147cedf1e29a247f0b6783101c8d3d0a4
Jul Jen-Prog <juliajen.prog@gmail.com>
2018-01-19 21:35:25 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -7,11 +7,19 @@ class Owner
 
   def initialize(species)
     @species = species
-    @@owners << self
+    # @@owners << self
   end
 
   def self.all
     @@owners
+  end
+
+  def self.reset_all
+    @@owners.clear
+  end
+
+  def self.count
+    @@owners.count
   end
 
 
